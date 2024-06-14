4a73bb26638b0f03fd81c62939f8a8b506d010dd
aakalaw <angeloaaronkalaw@gmail.com>
2024-06-14 14:33:06 +0800

    Update

---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/playerStats.css";
+
+const PlayerStats = () => {
+  return (
+    <div id="playerStats">
+      <h1>LOREM</h1>
+      <div id="playerStats__lines">
+        <span id="playerStats__lines__thick"></span>
+        <span id="playerStats__lines__thin"></span>
+      </div>
+      <h2>IPSUM</h2>
+    </div>
+  );
+};
+
+export default PlayerStats;
