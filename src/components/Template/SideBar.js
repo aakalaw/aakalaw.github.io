755feabcc748571afaff872734b85b691f3dab13
aakalaw <angeloaaronkalaw@gmail.com>
2024-06-16 19:41:36 +0800

    Update

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -19,7 +19,7 @@ const SideBar = () => (
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I am Angelo Aaron Kalaw.
+      <p>Hi, I am AAK.
       </p>
       <ul className="actions">
         <li>
