c3bc75f67e96dad44a9d5e0a088bab2d1623ea95
aakalaw <angeloaaronkalaw@gmail.com>
2024-06-16 16:38:07 +0800

    Update

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,7 +23,7 @@ const About = () => {
   return (
     <Main
       title="About"
-      description="Learn about Michael D'Angelo"
+      description="Learn about Lorem Ipsum"
     >
       <article className="post markdown" id="about">
         <header>
