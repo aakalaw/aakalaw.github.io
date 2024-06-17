63589c9c6f542b13155649c930a65591cd8855f6
aakalaw <angeloaaronkalaw@gmail.com>
2024-06-17 09:59:01 +0800

    Update

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -19,9 +19,9 @@ const SideBar = () => (
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I am Angelo Aaron Kalaw.  I am a TIP(QC) BSCpE undergraduate
-        and I have more than four years of work experience in CAD Operations
-        who wants to shift and cross the threshold of Web Development.
+      <p>Hi, I am Angelo Aaron Kalaw.  I am a proud TIPIAN and a BSCpE undergrad
+        but I have more than four years of work experience in CAD Operations
+        who wants to shift and cross the threshold of Software and Web Development.
       </p>
       <ul className="actions">
         <li>
--- src/data/projects.js
+++ src/data/projects.js
@@ -4,11 +4,11 @@ const data = [
     title: 'Fire Professionals Inc. Official Website',
     subtitle: '2023 Last Update',
     image: '/images/projects/nearestdollar.jpg',
-    date: '2023-01-01',
+    date: '2023-09-15',
     desc:
-      'Built  to generate more leads for local business online, sell products in an e-commerce store, '
-      + 'and to boost online engagements for products, services and business. Hosted in cpanel, '
-      + 'managed in WordPress, and edited using HTML+CSS.',
+      'Built to generate more leads and to boost online engagements '
+      + 'for local businesses about the services that the company offers. Hosted in cpanel, '
+      + 'managed in WordPress, and customized layout using HTML+CSS.',
   },
 ];
 
