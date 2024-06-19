d38f106a387a902aa9974d9a30a7d5b14522b809
aakalaw <angeloaaronkalaw@gmail.com>
2024-06-19 11:01:32 +0800

    Update

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("AAK");
+    expect(document.title).toBe('AAK');
   });
 
   it('can navigate to /about', async () => {
