d4d146640841148a9d75ea9496d1c51d7b1dda46
aakalaw <angeloaaronkalaw@gmail.com>
2024-06-19 11:14:15 +0800

    Update

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,12 +15,12 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'jsx-a11y/anchor-is-valid': ['error', {
-    'components': ['Link'],
-    'specialLink': ['to', 'hrefLeft', 'hrefRight'],
-    'aspects': ['noHref', 'invalidHref', 'preferButton'],
-    'linebreak-style': 0,
-    'global-require': 0,
-    'eslint linebreak-style': [0, 'error', 'windows'],
+      'components': ["Link"],
+      'specialLink': ["to", "hrefLeft", "hrefRight"],
+      'aspects': ["noHref", "invalidHref", "preferButton"],
+      'linebreak-style': 0,
+      'global-require': 0,
+      'eslint linebreak-style': [0, 'error', 'windows'],
     }],
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/href-no-hash': 'off',
