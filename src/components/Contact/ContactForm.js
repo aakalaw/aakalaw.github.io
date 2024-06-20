17009a302104d5f88b4895bdfa1109f250e8ccc9
aakalaw <angeloaaronkalaw@gmail.com>
2024-06-20 16:33:09 +0800

    Update

---
+++ src/components/Contact/ContactForm.js
@@ -0,0 +1,58 @@
+import React, { useRef } from 'react';
+
+import emailjs from '@emailjs/browser';
+
+const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const serviceID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+    const templateID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+    const userID = process.env.REACT_APP_EMAILJS_USER_ID;
+
+    emailjs
+      .sendForm(serviceID, templateID, form.current, userID)
+      .then(
+        () => {
+          alert('Message successfully sent!');
+          window.location.reload(false);
+        },
+        () => {
+          alert('Failed to send the message, please try again');
+        },
+      );
+  };
+
+  return (
+    <>
+      <div className="contact-form">
+        <form ref={form} onSubmit={sendEmail}>
+          <ul>
+            <input placeholder="Name" type="text" name="name" required />
+            <input
+              placeholder="Email"
+              type="email"
+              name="email"
+              required
+            />
+            <input
+              placeholder="Subject"
+              type="text"
+              name="subject"
+              required
+            />
+            <textarea
+              placeholder="Message"
+              name="message"
+              required
+            />
+            <input type="submit" className="flat-button" value="SEND" />
+          </ul>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default ContactForm;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Main from '../layouts/Main';
 import EmailLink from '../components/Contact/EmailLink';
 import ContactIcons from '../components/Contact/ContactIcons';
+import ContactForm from '../components/Contact/ContactForm';
 
 const Contact = () => (
   <Main
@@ -14,10 +15,12 @@ const Contact = () => (
       <header>
         <div className="title">
           <h2><Link to="/contact">Contact</Link></h2>
+          <p>Feel free to send me a direct message. I am here to assist you with any questions.</p>
+          <ContactForm />
         </div>
       </header>
       <div className="email-at">
-        <p>Feel free to get in touch. You can reach me via email at: </p>
+        <p>Or you can reach me via email at: </p>
         <EmailLink />
       </div>
       <ContactIcons />
