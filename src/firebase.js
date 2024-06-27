fc118874f5e769346df5cbada17a31a9debf9e9a
aakalaw <angeloaaronkalaw@gmail.com>
2024-06-27 22:06:45 +0800

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'DUMMY_API_KEY',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
