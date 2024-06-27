31592170e000c0bc7c5cb5fdc7d6778aa2007872
aakalaw <angeloaaronkalaw@gmail.com>
2024-06-27 17:40:58 +0800

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = getAuth();
 const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 export const storage = getStorage(app);
