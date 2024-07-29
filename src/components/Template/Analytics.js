b482bb734abcedfc9f5479d74b89bcc522d87b04
aakalaw <angeloaaronkalaw@gmail.com>
2024-07-29 09:25:04 +0800

    Update

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 const { NODE_ENV, REACT_APP_GA_MEASUREMENT_ID } = process.env;
 
@@ -13,7 +13,7 @@ const Analytics = () => {
       ReactGA.set({
         page: pathname,
       });
-      ReactGA.pageview(pathname);
+      ReactGA.send({ hitType: 'pageview', pathname });
     }
   }, [pathname]);
 
