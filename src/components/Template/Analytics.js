d5708e243d035db96fe3ec36c578ea198761dbda
aakalaw <angeloaaronkalaw@gmail.com>
2024-07-29 11:50:41 +0800

    Update

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 const { NODE_ENV, REACT_APP_GA_MEASUREMENT_ID } = process.env;
 
