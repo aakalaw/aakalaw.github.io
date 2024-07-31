097cb564dc4f60c200cd04f39f0c47a2cfd4c98d
aakalaw <angeloaaronkalaw@gmail.com>
2024-07-31 20:53:00 +0800

    Update

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -5,15 +5,12 @@ import ReactGA from 'react-ga4';
 const NODE_ENV = process.env;
 const GA_TRACKING_ID = process.env.REACT_APP_GA_MEASUREMENT_ID;
 
-if (NODE_ENV === 'production') {
-  ReactGA.initialize(GA_TRACKING_ID);
-}
-
 const Analytics = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
     if (NODE_ENV === 'production') {
+      ReactGA.initialize(GA_TRACKING_ID);
       ReactGA.set({
         page: pathname,
       });
