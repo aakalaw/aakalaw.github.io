90acef34de0b54472179d40ddf49e3185d0723dd
aakalaw <angeloaaronkalaw@gmail.com>
2024-07-31 21:46:42 +0800

    Update

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -2,8 +2,8 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
-const { NODE_ENV, REACT_APP_GA_MEASUREMENT_ID } = process.env;
-const GA_MEASUREMENT_ID = REACT_APP_GA_MEASUREMENT_ID;
+const { NODE_ENV } = process.env;
+const GA_MEASUREMENT_ID = process.env.REACT_APP_GA_MEASUREMENT_ID;
 
 const Analytics = () => {
   const { pathname } = useLocation();
