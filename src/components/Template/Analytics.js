c7cbcd43ddba3fb935a60381f60858cfa14a5626
aakalaw <angeloaaronkalaw@gmail.com>
2024-07-31 21:34:11 +0800

    Update

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -2,19 +2,20 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
-const { NODE_ENV, REACT_APP_GA_MEASUREMENT_ID } = process.env;
+const NODE_ENV = process.env;
+const GA_MEASUREMENT_ID = process.env.REACT_APP_GA_MEASUREMENT_ID;
 
 const Analytics = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
     if (NODE_ENV === 'production') {
-      if (!REACT_APP_GA_MEASUREMENT_ID) {
+      if (!GA_MEASUREMENT_ID) {
         console.error('Error: GA_MEASUREMENT_ID is not defined');
         return;
       }
       
-      ReactGA.initialize(REACT_APP_GA_MEASUREMENT_ID);
+      ReactGA.initialize(GA_MEASUREMENT_ID);
       ReactGA.set({ page: pathname });
       ReactGA.send({ hitType: 'pageview', pathname });
     }
