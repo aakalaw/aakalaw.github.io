eb5437af24f2a610d418a3892cee9dbf1bbc4864
aakalaw <angeloaaronkalaw@gmail.com>
2024-08-01 13:34:01 +0800

    Update

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -8,7 +8,7 @@ const Analytics = () => {
   useEffect(() => {
     const GA_MEASUREMENT_ID = process.env.REACT_APP_GA_MEASUREMENT_ID;
 
-    if (process.env.NODE_ENV === 'production' && GA_MEASUREMENT_ID) {
+    if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_GA_MEASUREMENT_ID) {
       ReactGA.initialize(GA_MEASUREMENT_ID);
       ReactGA.set({
         page: pathname,
